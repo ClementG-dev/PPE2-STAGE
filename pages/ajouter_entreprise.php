3343230e0d2052f4e728ae5a516cceda4505c720
Quentin Quéro <quentin.quero22@gmail.com>
2020-02-20 18:07:57 +0100

    Restructuration (problème navbar-top fonctionne que dans l'index)

---
+++ pages/ajouter_entreprise.php
@@ -0,0 +1,25 @@
+<?php include('../head.php'); ?>
+<div class="container-fluid">
+    <div class="row" >
+        <div class="col-sm" >
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Ajouter une entreprise</h5>
+                    <p class="card-text">Veuillez entrez les infos suivantes : </p>
+                    <form method="POST" action="ajouter_entreprise.php" >
+                        <div class="row">
+                            <div class="col">
+                                <input type="text" class="form-control etu" name="address" placeholder="Adresse" requierd>
+                                <input type="text" class="form-control etu" name="nom" placeholder="Nom">
+                                <input type="text" class="form-control etu" name="phone_number" placeholder="Numéro de téléphone">
+                                <input type="text" class="form-control etu" name="mail" placeholder="Mail">
+                            </div>
+                        </div>
+                        <input class="btn btn-primary addEntreprise" type="submit" ></input>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include(BASE_PATH.'/skeleton/footer.php'); ?>
---
+++ skeleton/footer.php
@@ -0,0 +1,8 @@
+ </body>
+ <footer>
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+     <script src="bootstrap/js/bootstrap.min.js"></script>
+ </footer>
+
+</html>
+
