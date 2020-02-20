9a7e3a4413950f76edab1e5b8a4a92fde83ccf55
Clément <cgazeau.ledantec@gmail.com>
2020-02-20 13:44:35 +0100

    Ne necessite plus d'etre connecté

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
 
-<?php session_start();
-
-  if($_SESSION['nom'] == "")
-  {
-    header('Location: login.php');
-  }
-
-?>
 
 <!doctype html>
 <html lang="en">
@@ -16,7 +8,6 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Jekyll v3.8.6">
-    <title>FLD GS - Prénom nom</title>
 
     <link rel="stylesheet" type="text/css" href="assets\css\main.css">
 
@@ -31,50 +22,56 @@
   </head>
   <body>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-  <a class="navbar-brand" href="#">FLD Gestion de Stages</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarCollapse">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Entreprises</a>
-        <div class="dropdown-menu">
-            <a class="dropdown-item" href="#">Ajouter une entreprise</a>
-            <a class="dropdown-item" href="liste_entreprise.php">Liste entreprise</a>
+      <a class="navbar-brand" href="#">FLD Gestion de Stages</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Entreprises</a>
+            <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">Ajouter une entreprise</a>
+                <a class="dropdown-item" href="liste_entreprise.php">Liste entreprise</a>
+            </div>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Etudiant</a>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="ajouter_etudiant.php">Ajouter étudiant</a>
+              <a class="dropdown-item" href="modifier">Modifier étudiant</a>
+              <a class="dropdown-item" href="#">Liste étudiant</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Stage <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Visite <span class="sr-only">(current)</span></a>
+          </li>
+        </ul> 
+        <div class="form-inline mt-2 mt-md-0">
+        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Profil</a>
+          <div class="dropdown-menu">
+              <a class="dropdown-item" href="#"></a>
+              <a class="dropdown-item" href="#"></a>
+              <a class="dropdown-item" href="#"></a>
+            </div>
         </div>
-      </li>
-        <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Etudiant</a>
-        <div class="dropdown-menu">
-            <a class="dropdown-item" href="#">Ajouter étudiant</a>
-            <a class="dropdown-item" href="#">Modifier étudiant</a>
-        </div>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="#">Stage <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Visite <span class="sr-only">(current)</span></a>
-      </li>
-    </ul> 
-    <div class="form-inline mt-2 mt-md-0">
-      <a class="nav-link" href="#">Profil <span class="sr-only">(current)</span></a>
-    </div>
-  </div>
+      </div>
     </nav>
+
+    <main role="main" class="container">
+      <div class="jumbotron">
+        <h1>Il vous reste 36 Jours avant de trouver un stage</h1>
+        <p class="lead">Vous avez déja cotacté 3 entreprises</p>
+        <a class="btn btn-lg btn-primary" href="/docs/4.4/components/navbar/" role="button">View navbar docs &raquo;</a>
+      </div>
+    </main>
   </body>
-<main role="main" class="container">
-  <div class="jumbotron">
-    <h1>Il vous reste 36 Jours avant de trouver un stage</h1>
-    <p class="lead">Vous avez déja cotacté 3 entreprises</p>
-    <a class="btn btn-lg btn-primary" href="/docs/4.4/components/navbar/" role="button">View navbar docs &raquo;</a>
-  </div>
-</main>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="bootstrap/js/bootstrap.min.js"></script>
 </html>
