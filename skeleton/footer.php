750c884adc21d8c9d1eaa32eda9ec71d7919ee93
Max <maxime.quero@gmail.com>
2020-03-06 17:54:28 +0100

    * Changing call to boostrap javascript (which handle menu dropdown) so it is absolute and not relative, so it can work from every folder of the project

--- skeleton/footer.php
+++ skeleton/footer.php
@@ -1,7 +1,7 @@
  </body>
  <footer>
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-     <script src="bootstrap/js/bootstrap.min.js"></script>
+     <script src="<?php echo BASE_URL ?>/bootstrap/js/bootstrap.min.js"></script>
  </footer>
 
 </html>
