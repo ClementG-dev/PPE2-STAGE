6d5a457b93dbda9ef57df165f57df837a849fb05
Max <maxime.quero@gmail.com>
2020-03-06 17:31:58 +0100

    * Change BASE_URL so it is the good one * Calling CSS an bootstrap files from BASE URL

--- head.php
+++ head.php
@@ -1,29 +1,32 @@
+<?php
+//declare constante at root project for having the base path
+//DIR = root at this moment
+define("BASE_PATH", __DIR__);
+//define("BASE_URL", "http://" . $_SERVER['SERVER_NAME'] . '/PPE2-STAGE');
+//You may have to modify this if your base url is different (ex: if you use PHP local server)
+define("BASE_URL", "http://" . $_SERVER['HTTP_HOST'] . '/PPE2-STAGE');
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Jekyll v3.8.6">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+	<meta name="generator" content="Jekyll v3.8.6">
 
-    <link rel="stylesheet" type="text/css" href="http://localhost/PPE2-STAGE/assets/css/main.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL ?>/assets/css/main.css">
 
-    <!-- Bootstrap core CSS -->
-    <link href="http://localhost/PPE2-STAGE/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Favicons -->
+	<!-- Bootstrap core CSS -->
+	<link href="<?php echo BASE_URL ?>/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<!-- Favicons -->
 
-    <!-- Custom styles for this template -->
+	<!-- Custom styles for this template -->
 
 </head>
 
 <?php
-//declare constante at root project for having the base path
-//DIR = root at this moment
-
-define("BASE_PATH", __DIR__);
-define("BASE_URL", '/PPE2-STAGE');
 include(__DIR__.'/skeleton/header.php');
-
 ?>
 <body>
--- skeleton/menu.php
+++ skeleton/menu.php
@@ -7,7 +7,7 @@
     <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-                <a class="nav-link" href="<?php echo(BASE_URL) ?>/">Accueil <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="<?php echo(BASE_URL) ?>/index.php">Accueil <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Entreprises</a>
